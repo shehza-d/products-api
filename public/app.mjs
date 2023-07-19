4199eff6cce61e1979c74acf14ca453d76a87471
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-20 00:16:31 +0500

    front end js separated and notyf toast added

---
+++ public/app.mjs
@@ -0,0 +1,75 @@
+const baseUrl = `http://localhost:3003`;
+const productName = document.querySelector("#productName");
+const price = document.querySelector("#price");
+const description = document.querySelector("#description");
+const dataDiv = document.querySelector("#data");
+const form = document.querySelector("#addProductForm");
+
+// notyf.success("data.message");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const { data } = await axios.post(`${baseUrl}/api/v1/product`, {
+      name: productName.value,
+      price: price.value,
+      description: description.value,
+    });
+    notyf.success(data.message);
+    getProductData();
+    form.reset();
+  } catch (err) {
+    notyf.error(err.response.data.message);
+  }
+});
+
+const getProductData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`${baseUrl}/api/v1/products`);
+
+    console.log("🚀 getProductData ~ data:", data);
+    if (!data.length) throw Error("No Data Found");
+
+    data.map((item, i) => {
+      dataDiv.innerHTML += `
+</div>
+  <div class="border relative p-4 rounded-lg border-slate-700">
+  <h3 class="text-2xl font-semibold capitalize">${item.name}</h3>
+  <h5 class="text-xl">$ ${item.price}</h5>
+  <p class="text-justify font-light">
+    ${item.description}
+  </p>
+  <span
+	class="absolute top-4 right-6 bg-blue-400 rounded-full h-7 flex justify-center items-center w-7"
+	>${i + 1}
+  </span>
+</div>
+  `;
+    });
+  } catch (err) {
+    notyf.error(err.response.data.message);
+    console.log("🚀 ~ file: index.html:84 ~ getProductData ~ err:", err);
+  }
+};
+getProductData();
+
+const notyf = new Notyf({
+  duration: 4000,
+  position: {
+    x: "center",
+    y: "top",
+  },
+  dismissible: true,
+  types: [
+    {
+      type: "success",
+      background: "#22c55e",
+    },
+    {
+      type: "error",
+      background: "#ef4444",
+    },
+  ],
+});
